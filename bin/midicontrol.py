db6da9b5cd681300b280581aada5f4f32707cb79
Jon Nordby <jononor@gmail.com>
2017-06-06 23:02:09 +0200

    Send MIDI messages over MQTT, and MsgFlo discovery message
    ./bin/midicontrol.py --iips '{"open": "LPD8"}'

--- bin/midicontrol.py
+++ bin/midicontrol.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python2
 
 import mido
+import gevent
+import msgflo
+
+import sys, os, json, logging
+
+def serialize_midi(msg):
+    return msg.dict()
+
+class MidiInput(msgflo.Participant):
+  def __init__(self, role):
+    d = {
+      'component': 'midicontrol/Input',
+      'label': 'Sends input from a MIDI device',
+      'inports': [
+        { 'id': 'open', 'type': 'string', 'hidden': True },
+      ],
+      'outports': [
+        { 'id': 'raw', 'type': 'object' },
+      ],
+    }
+    msgflo.Participant.__init__(self, d, role)
+
+  def open(self, devicename):
+    device = find_input(devicename)
+
+    self.inport = inport = mido.open_input(device)
+    def read_inport():
+        while inport:
+            for m in inport.iter_pending():
+                raw = serialize_midi(m)
+                self.send('raw', raw)
+    gevent.spawn(read_inport)
+
+  def process(self, inport, msg):
+    if inport == 'open':
+        name = msg.data
+        self.open(name)
+        #self.ack(msg)
+        #self.send('raw', {'opened': name})
 
 def find_input(want):
     device = None
@@ -11,13 +50,7 @@ def find_input(want):
     return device
 
 def main():
-    want = 'LPD8'
-    device = find_input(want)
-
-    with mido.open_input(device) as inport:
-        print 'opened'
-        for msg in inport:
-            print(msg)
+    msgflo.main(MidiInput)
 
 if __name__ == '__main__':
     main()
